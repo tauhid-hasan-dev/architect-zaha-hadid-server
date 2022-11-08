9013a314e5f53c92cf3251197acdc0a0237af12c
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-11-08 15:01:52 +0300

    vercel deply completed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
+/* var jwt = require('jsonwebtoken'); */
 
 const port = process.env.PORT || 5000;
 
