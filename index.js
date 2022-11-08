63607734fb32aa6df45ab983d6dd727e656bc41c
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-11-08 14:54:35 +0300

    sever created, and express, cors , mongo, dotenv installed

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Server running for Architect Zaha Hadid');
+})
+
+app.listen(port, () => {
+    console.log(`Server running for Architect Zaha Hadid on port ${port}`);
+})
