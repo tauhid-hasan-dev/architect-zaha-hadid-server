1c005f566f5cd9c5d91a390b5a1fb01cf901511a
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-11-09 12:26:32 +0300

    delete api created and api has been created

--- index.js
+++ index.js
@@ -75,6 +75,14 @@ async function run() {
         })
 
 
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
 
     } finally {
 
